d6fbf8876ae8abc0141596a87e611807a4132015
Mohit <trickytreat33@gmail.com>
2026-07-14 09:59:54 +0530

    Add files via upload

---
+++ 07_operators/01_arithmetic_operators.py
@@ -0,0 +1,24 @@
+# In these pages we will talk about the Operators in Python.
+# There are multiple types of operators in Python
+
+# 1 - Arithmetic operators, they are used to perform arithmetic operations ex- add, subtract, divide ,multiply, etc.
+a = 65
+
+b = 3
+
+c = 2
+
+print("a + b = ", a+b)
+print("a - b = ", a-b)
+print("a * b = ", a*b)
+print("a / b = ", a/b)
+print("a % b = ", a%b) # % - Percentage, here you will get the remainder
+print("a // b  = ", a//b) # // - Floor division,  it just remove the decimals, very much similar to divide/division
+print("a ** b = ", a**b) # ** - exponentian operator, in this case a = 65 and b = 3, means 65*65*65=274625
+
+# Inside double quotes " is a string, while there is another argument which is an integer.
+
+print("a + b - c = ", a+b-c)
+print("a * c - b = ", a*c-b)
+print("a / b - b = ", a/b-b)
+print("b ** c + a = ", b**c+a)
---
+++ 07_operators/02_conditional_operators.py
@@ -0,0 +1,18 @@
+# 2 - Comparision/Conditional operator, it always gives you the output in true or false, similar to boolean.
+
+a = 100
+
+b = 50
+
+# Examples of conditional operator.
+print(a > b)
+print(a < b)
+print(a < 101)
+print(a == 101) #Equals to sign, cause a single equal(=) to is an assignment operator it helps to stores the value. 
+print(a != 101) #Not equal sign, means a is not equal to 101
+print(a >= 101) # Greater or equal sign
+print(a <= 101) # Less than or equal sign
+print(a == b)
+print(a==b, b==a, b==1001,)
+print(a==b==1001)
+# You can write multiple conditional operators as much as you can,, you can mix up and can use multiple variables and values.
