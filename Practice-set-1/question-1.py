875371555706223bac38d45753a48d1a2cd5a853
Mohit <trickytreat33@gmail.com>
2026-07-15 00:04:31 +0530

    Add files via upload

---
+++ Practice-set-1/question-1.py
@@ -0,0 +1,9 @@
+# Q1: Your First Program
+
+# Write a program that prints:
+'''
+Hello, World! Welcome to Python.
+'''
+
+# Solution:
+print("Hello, World! Welcome to Python.")
---
+++ Practice-set-1/question-3.py
@@ -0,0 +1,21 @@
+# Q3: Variables & Data Types
+'''
+Your name (string)
+Your age (integer)
+Your height in meters (float)
+A boolean value representing whether you are a student
+
+Print all of them in one line.
+'''
+
+# Solution:
+# print("Mohit Rajpal", 22, 1.778, True, sep=" , ")
+
+# I am not sure about the boolean value, for that i will just type true, tomorrow when i will watch the video and then i will correct it, if wrong.
+# NOTE: I have see the answer, and what i have done is totally wrong. 
+
+name = "Mohit"
+age = 22
+height = 1.778
+is_student = True
+print(f"The name of the student is {name}, he is {age} years old. his height is around {height} meter, and yes he is a student: {is_student}")
