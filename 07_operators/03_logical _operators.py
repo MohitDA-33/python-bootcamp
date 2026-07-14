35ccdc039dd8207be3fc6f90e48839d8266c879b
Mohit <trickytreat33@gmail.com>
2026-07-14 10:00:21 +0530

    Add files via upload

---
+++ 07_operators/03_logical _operators.py
@@ -0,0 +1,25 @@
+# 3 - Logical operators are used to perform operations on booleans
+# and, or, not operator
+
+# Examples of logical operators
+a = True
+b = False
+
+# and operator
+print(a and b) 
+print(a and a)
+print(b and a)
+print(b and b)
+# and operator always prints true if both of them are true otherwise false.
+
+# or operator
+print(a or b) 
+print(a or a)
+print(b or a)
+print(b or b)
+# or operator prints true everytime if even one of them is true, kind of opposite to and operator.
+
+# not operator
+print(not(True)) 
+print(not(False))
+# Its going to print the opposite of what the value is given. it helps when you want to negate the value
---
+++ 07_operators/04_assignment_operators.py
@@ -0,0 +1,33 @@
+# 4 - Assignment operators, they are used to perform operations on the assignment or on the equals to sign(=).
+
+# Examples of assignment operators
+a = 50
+print(a)
+
+b = 100
+b +=5     # Equivalent to x = x + 5 , so x = 100, 100 + 5 = 105.
+print(b)
+
+c = 101
+c -= 50
+print(c)
+
+d = 500
+d %= 5
+print(d)
+
+e = 10
+e *= 10
+print(e)
+
+f = 300
+f /= 10
+print(f)
+
+g = 500
+g //= 50
+print(g)
+
+h = 2
+h **= 2
+print(h)
