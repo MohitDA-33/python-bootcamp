0e8a7b6b30ee8928281a3e625a06482f7cd7c99d
Mohit <trickytreat33@gmail.com>
2026-07-15 10:03:19 +0530

    Add files via upload

---
+++ Practice-set-1/question-8.py
@@ -0,0 +1,17 @@
+# Q8: Operator Challenge
+
+'''
+Write a program that:
+
+Takes an integer as input from the user.
+Prints the square and cube of that number.
+'''
+
+# Solution
+
+a = int(input("Enter your Number: "))
+
+print("a ** 2 = ", a**2)    #Square
+print("a ** 3 = ", a**3)    #Cube
+
+# What it does is it takes integer as an input from the user, then it uses exponentian operator(**) to solve it, to get the square and cube of that integer.
---
+++ Practice-set-1/question-9.py
@@ -0,0 +1,18 @@
+# Q9: Quick Quiz (True/False)
+
+# Mark each as True or False:
+'''
+Python code must always end with a semicolon ;                     #False
+
+The # symbol is used for comments in Python                        #True
+
+"123" and 123 are the same in Python                               #False
+
+The * operator is used for multiplication                          #True
+
+\n creates a new line                                              #True
+
+Variables in Python can start with numbers                         #False
+
+int("10") + 5 gives 15                                             #True
+'''
