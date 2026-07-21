72622797657df42b94b2311feab99f953d2f7ab1
Mohit <trickytreat33@gmail.com>
2026-07-21 08:30:47 +0530

    Add files via upload

---
+++ Practice-set-2/problem1.1.py
@@ -0,0 +1,14 @@
+# If-Else Conditional Statements
+
+# 1 - Write a program that asks the user for a number and prints whether it is positive, negative, or zero.
+
+num = int(input("Enter a number: "))
+
+if num > 0:
+    print("Number is positive!")
+
+elif num < 0:
+    print("Number is negative!")
+
+else :
+    print("Number is zero")
---
+++ Practice-set-2/problem1.3.py
@@ -0,0 +1,13 @@
+# 3 - Write a program that takes a number from the user and prints "Even" if it is even, otherwise "Odd"
+
+num = int(input("Enter a number: "))
+
+if num%2 == 0:
+    print("Its an even number")
+
+# If a number leaves 0 as an remainder after being divisible by 2, means it's an even number
+
+else :
+    print("Its a odd number!")
+
+# And if the number is not divisible by 2, its an odd number.
